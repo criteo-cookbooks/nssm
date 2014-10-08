b2f63df0a2559be383567c6a53f44998e6dcfbc6
Dennis Hoer <dennis.hoer@pearson.com>
2014-10-08 15:09:38 -0700

    Fix rubocop issues

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,7 +14,7 @@ system = node['kernel']['machine'] == 'x86_64' ? 'win64' : 'win32'
 
 batch 'copy_nssm' do
   code <<-EOH
-    xcopy #{Chef::Config[:file_cache_path].gsub("/","\\")}\\#{basename}\\#{system}\\nssm.exe %WINDIR% /y
+    xcopy #{Chef::Config[:file_cache_path].gsub('/', '\\')}\\#{basename}\\#{system}\\nssm.exe %WINDIR% /y
   EOH
   not_if { ::File.exist?('c:\\windows\\nssm.exe') }
 end
