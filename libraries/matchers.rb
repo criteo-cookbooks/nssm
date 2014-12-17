9905c89d6e074fd1f1f81d0611e78ce1e71e626b
Dennis Hoer <dennis.hoer@pearson.com>
2014-12-17 16:54:13 -0700

    Remove deprecated matchers

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -6,14 +6,4 @@ if defined?(ChefSpec)
   def remove_nssm(servicename)
     ChefSpec::Matchers::ResourceMatcher.new(:nssm, :remove, servicename)
   end
-
-  # deprecated: use install_nssm
-  def install_nssm_service(servicename)
-    ChefSpec::Matchers::ResourceMatcher.new(:nssm, :install, servicename)
-  end
-
-  # deprecated: use remove_nssm
-  def remove_nssm_service(servicename)
-    ChefSpec::Matchers::ResourceMatcher.new(:nssm, :remove, servicename)
-  end
 end
