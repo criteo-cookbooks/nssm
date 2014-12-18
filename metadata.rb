32000a580e190b3083a52da7b3fdfbb4f9e8f3da
Dennis Hoer <dennis.hoer@pearson.com>
2014-12-17 17:00:34 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures NSSM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '1.0.0'
 
 supports 'windows'
 
