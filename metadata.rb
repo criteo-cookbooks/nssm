78a4b463425f4575a11e4aacb65ee3ebaeb7397f
Dennis Hoer <dennis.hoer@pearson.com>
2015-02-06 10:51:49 -0700

    Update license to MIT

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'nssm'
 maintainer 'Dennis Hoer'
 maintainer_email 'dennis.hoer@gmail.com'
-license 'All rights reserved'
+license 'MIT'
 description 'Installs/Configures NSSM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.0'
