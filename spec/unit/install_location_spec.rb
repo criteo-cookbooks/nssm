67558ad34d81cc454b1f52b6a21a1b2192aa5509
Graham Davison <gdavison@paybyphone.com>
2015-02-05 16:17:02 -0800

    Add an attribute for the install location of nssm.exe

---
+++ spec/unit/install_location_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'nssm::default' do
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new(platform: 'windows', version: '2008R2') do |node|
+      node.set['nssm']['install_location'] = 'c:\somewhere'
+    end.converge(described_recipe)
+  end
+
+  it 'copies nssm executable' do
+    expect(chef_run).to run_batch('copy_nssm').with(
+        code: /xcopy .*\\nssm-2.24\\win64\\nssm.exe "c:\\somewhere" \/y/)
+  end
+end
