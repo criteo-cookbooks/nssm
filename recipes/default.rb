37317877d2a2d5aaa1fa0f4945a131398792dd97
Dennis Hoer <dennis.hoer@pearson.com>
2015-08-08 23:52:06 -0700

    Fix guard check

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,11 +15,12 @@ if platform?('windows')
 
   batch 'copy_nssm' do
     code <<-EOH
-      xcopy #{Chef::Config[:file_cache_path].tr('/', '\\')}\\#{basename}\\#{system}\\nssm.exe \
+      xcopy "#{Chef::Config[:file_cache_path].tr('/', '\\')}\\#{basename}\\#{system}\\nssm.exe" \
 "#{node['nssm']['install_location']}" /y
     EOH
-    not_if { ::File.exist?("#{node['nssm']['install_location']}\\nssm.exe") }
+    not_if { ::File.exist?("#{node['nssm']['install_location']}\\nssm.exe".gsub(/%[^%]+%/) { |m| ENV[m[1..-2]] } ) }
   end
 else
   log('NSSM can only be installed on Windows platforms!') { level :warn }
 end
+
