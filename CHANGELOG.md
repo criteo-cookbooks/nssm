fb54755e3e6bdb5adec66d38fda1f7e3796dea5f
Dennis Hoer <dennis.hoer@pearson.com>
2015-08-09 06:20:15 -0700

    Update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # NSSM CHANGELOG
 
+## 1.2.0
+
+- Use new install_location attribute everywhere
+- Don't try to install nssm if it's already there
+
 ## 1.1.0
 
 - Add an attribute for the install location of nssm.exe
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description 'Installs/Configures NSSM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.0'
+version '1.2.0'
 
 supports 'windows'
 
