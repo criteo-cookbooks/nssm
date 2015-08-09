f3b7c8d0955d6cbfceee6f3951801eb0978c86b9
Dennis Hoer <dennis.hoer@pearson.com>
2015-08-09 06:39:26 -0700

    Fix rubocop offenses

--- test/integration/nssm_test/serverspec/default_spec.rb
+++ test/integration/nssm_test/serverspec/default_spec.rb
@@ -2,6 +2,6 @@ require 'serverspec_helper'
 
 describe 'selenium::server' do
   describe host('localhost') do
-    it { should be_reachable.with( :port => 4444 ) }
+    it { should be_reachable.with(port: 4444) }
   end
 end
