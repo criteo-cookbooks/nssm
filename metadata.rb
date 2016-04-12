f109c03a8c80081afbb5f316d6b1124c9e35bcd2
Dennis Hoer <dennis.hoer@pearson.com>
2016-04-12 11:51:40 -0700

    Bump version and add issues_url and source_url

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,11 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description 'Installs/Configures NSSM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.0'
+version '1.2.1'
 
 supports 'windows'
 
 depends 'windows', '~> 1.0'
+
+source_url 'https://github.com/dhoer/chef-nssm' if respond_to?(:source_url)
+issues_url 'https://github.com/dhoer/chef-nssm/issues' if respond_to?(:issues_url)
