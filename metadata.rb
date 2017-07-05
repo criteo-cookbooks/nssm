fce9b4f40e4925277ed3e84af7d1bd1b3b112551
Patrick O'Brien <obrien.patrickl@gmail.com>
2017-07-05 13:42:38 -0700

    Fixes for Issue #12
    This commit does the following:
    
    * Sets the minimum supported chef client version to 12.7
    * Sets the windows cookbook version to 3.1
    * Updates test kitchen to use chef_zero instead of chef_solo
    * Fixes an issue in recipes/default.rb where the file URI needed 1 more slash.

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description 'Installs/Configures NSSM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/dhoer/chef-nssm'
 issues_url 'https://github.com/dhoer/chef-nssm/issues'
-version '3.0.0'
+version '4.0.0'
 
-chef_version '>= 12.6'
+chef_version '>= 12.7'
 
 supports 'windows'
 depends 'windows'
