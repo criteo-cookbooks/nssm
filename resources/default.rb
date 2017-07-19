d51f0f466f7b1aa2b6d0fef209163425f5696cea
Baptiste Courtois <b.courtois@criteo.com>
2017-07-19 22:10:06 +0200

    Rewrite nssm resource into a custom resource

--- resources/default.rb
+++ resources/default.rb
@@ -3,8 +3,94 @@
 actions :install, :install_if_missing, :remove
 default_action :install
 
-attribute :servicename, name_attribute: true
-attribute :program, kind_of: String, required: true
-attribute :args, kind_of: String
-attribute :parameters, kind_of: Hash, default: {}
-attribute :start, kind_of: [TrueClass, FalseClass], default: true
+provides 'nssm'
+
+property :servicename, name_attribute: true
+property :program, kind_of: String, required: true
+property :args, kind_of: String
+property :parameters, kind_of: Hash, default: lazy { ::Mash.new }
+property :start, kind_of: [TrueClass, FalseClass], default: true
+
+action :install_if_missing do
+  return Chef::Log.warn('NSSM service can only be installed on Windows platforms!') unless platform?('windows')
+
+  install_nssm
+  service_installed = ::Win32::Service.exists? new_resource.servicename
+
+  execute "Install #{new_resource.servicename} service" do
+    command "#{nssm_exe} install \"#{new_resource.servicename}\" \"#{new_resource.program}\" #{new_resource.args}"
+    not_if { service_installed }
+  end
+
+  new_resource.parameters.map do |k, v|
+    execute "Set parameter #{k} #{v}" do
+      command "#{nssm_exe} set \"#{new_resource.servicename}\" #{k} \"#{v.gsub('"', '^"').strip}\""
+      not_if { service_installed }
+    end
+  end
+
+  service new_resource.servicename do # ~FC021
+    action [:start]
+    only_if { new_resource.start }
+  end
+end
+
+action :install do
+  return Chef::Log.warn('NSSM service can only be installed on Windows platforms!') unless platform?('windows')
+
+  install_nssm
+  service_installed = ::Win32::Service.exists? new_resource.servicename
+
+  execute "Install #{new_resource.servicename} service" do
+    command "#{nssm_exe} install \"#{new_resource.servicename}\" \"#{new_resource.program}\" #{new_resource.args}"
+    not_if { service_installed }
+  end
+
+  parameters = new_resource.parameters.merge(
+    'Application' => new_resource.program,
+    'AppParameters' => new_resource.args
+  )
+
+  parameters.map do |k, v|
+    value = v.to_s.gsub('"', '^"').strip
+    execute "Set parameter #{k} to #{value}" do
+      command "#{nssm_exe} set \"#{new_resource.servicename}\" #{k} \"#{value}\""
+      not_if "#{nssm_exe} get \"#{new_resource.servicename}\" #{k} | findstr /BEC:\"#{value}\""
+    end
+  end
+
+  service new_resource.servicename do # ~FC021
+    action [:start]
+    only_if { new_resource.start }
+  end
+end
+
+action :remove do
+  if platform?('windows')
+    service_installed = ::Win32::Service.exists? new_resource.servicename
+
+    execute "Remove service #{new_resource.servicename}" do
+      command "#{nssm_exe} remove \"#{new_resource.servicename}\" confirm"
+      only_if { service_installed }
+    end
+  else
+    Chef::Log.warn('NSSM service can only be removed from Windows platforms!')
+  end
+end
+
+action_class do
+  def whyrun_supported?
+    true
+  end
+
+  def install_nssm
+    return if run_context.loaded_recipe? 'nssm::default'
+    recipe_eval do
+      run_context.include_recipe 'nssm::default'
+    end
+  end
+
+  def nssm_exe
+    "#{node['nssm']['install_location']}\\nssm.exe"
+  end
+end
