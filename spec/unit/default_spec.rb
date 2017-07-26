02907308720f425d0a56d9852a74e902b765d518
axelrtgs <jamie@axelrtgs.com>
2017-07-26 13:36:07 -0400

    Updated tests to reflect new resource

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -6,12 +6,19 @@ describe 'nssm::default' do
   context 'windows' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(
-        file_cache_path: CACHE, platform: 'windows', version: '2008R2'
+        file_cache_path: CACHE, platform: 'windows', version: '2008R2', step_into: ['nssm_install']
       ) do
         ENV['WINDIR'] = 'C:\tmp'
       end.converge(described_recipe)
     end
 
+    it 'calls nssm_install resource' do
+      expect(chef_run).to install_nssm_install('Install NSSM').with(
+        source: "https://nssm.cc/ci/nssm-2.24-94-g9c88bc1.zip",
+        sha256: "0bbe25025b69ebd8ab263ec4b443513d28a0d072e5fdd9b5cdb327359a27f96e"
+      )
+    end
+    
     it 'download nssm' do
       expect(chef_run).to unzip_windows_zipfile('download nssm').with(
         path: CACHE,
