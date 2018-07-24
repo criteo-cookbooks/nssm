c5d44a86f9c248a6d16ce49d57c5d9d7ccc90efb
axelrtgs <jamie@axelrtgs.com>
2018-07-24 17:21:40 -0400

    Bug fix release 4.0.1 (#37)
    Prepare for release 4.0.1

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description 'Installs/Configures NSSM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/dhoer/chef-nssm'
-issues_url 'https://github.com/dhoer/chef-nssm/issues'
-version '4.0.0'
+source_url 'https://github.com/criteo-cookbooks/nssm'
+issues_url 'https://github.com/criteo-cookbooks/nssm/issues'
+version '4.0.1'
 
 chef_version '>= 12.7'
 
