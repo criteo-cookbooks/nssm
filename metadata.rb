3de38922f57810656062aa38c3af1023d026c0f1
Jonathan Amiez <j.amiez@criteo.com>
2021-11-19 08:45:32 +0100

    Set unified_mode on custom resources
    Rework nssm_install resource to remove :before notification which does
    not work in unified mode in that case.

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,10 @@ maintainer 'Criteo'
 maintainer_email 'systems-services-team@criteo.com'
 license 'MIT'
 description 'Installs/Configures NSSM'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/criteo-cookbooks/nssm'
 issues_url 'https://github.com/criteo-cookbooks/nssm/issues'
-version '5.0.0'
+version '5.1.0'
 
-chef_version '>= 15.0'
+chef_version '>= 15.3'
 
 supports 'windows'
--- resources/install_noop.rb
+++ resources/install_noop.rb
@@ -1,5 +1,7 @@
 provides :nssm_install
 
+unified_mode true
+
 property :source, String, identity: true, name_property: true
 property :sha256, String, required: true
 
@@ -7,10 +9,4 @@ default_action :install
 
 action :install do
   ::Chef::Log.warn('NSSM service can only be installed on Windows platforms!')
-end
-
-action_class do
-  def whyrun_supported?
-    true
-  end
 end
